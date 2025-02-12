5f55ce5538ead933a84039f36230e2f622df2666
kimura <clookout@gmail.com>
2025-02-12 19:06:58 +0900

    0211-front-2

--- src/app/customers/create/confirm/fetchCustomer.js
+++ src/app/customers/create/confirm/fetchCustomer.js
@@ -1,5 +1,5 @@
 export default async function fetchCustomer(id) {
-  const res = await fetch(`http://localhost:8000/customers?customer_id=${id}`, {
+  const res = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT + `/customers?customer_id=${id}`, {
     cache: "no-cache",
   });
   if (!res.ok) {
